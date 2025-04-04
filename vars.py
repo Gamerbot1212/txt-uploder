dfe31faf0fb3146d57ad7476627a67fb1344ec8b
ROHITMEENA64 <157285469+ROHITMEENA64@users.noreply.github.com>
2025-04-04 10:56:54 +0530

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,8 @@
+# Don't Remove Credit Tg - 😈 lucifer 😈 
+# Ask Doubt on telegram 😈 lucifer 😈 
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "27775431"))
+API_HASH = environ.get("API_HASH", "b70bb1d45a1d05236671d4cc615e40f9")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
