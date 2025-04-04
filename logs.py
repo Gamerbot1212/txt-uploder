74639c40a3001d4d9af90f4ff1f8678d92caa2a0
ROHITMEENA64 <157285469+ROHITMEENA64@users.noreply.github.com>
2025-04-04 10:44:07 +0530

    Create logs.py

---
+++ logs.py
@@ -0,0 +1,20 @@
+# Don't Remove Credit Tg - 😈 lucifer 😈 
+# Ask Doubt on telegram 😈 lucifer 😈 
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+logging.basicConfig(
+    level=logging.ERROR,
+    format=
+    "%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler("logs.txt", maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+logging = logging.getLogger()
